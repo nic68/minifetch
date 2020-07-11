8decabb092660884c113d97fc34d0d81240fe87f
Nick <53533645+nic68@users.noreply.github.com>
2020-07-11 15:44:16 +0200

    main file

---
+++ minifetch.py
@@ -0,0 +1,27 @@
+#!/data/data/com.termux/files/usr/bin/python3
+from os import system as sy
+try:
+	import psutil
+except Exception:
+	sy("pip3 install psutil")
+	import psutil
+import platform
+def conv(bytes, suffix="B"):
+    fact = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < fact:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= fact
+inf = platform.uname()
+mem = psutil.virtual_memory()
+frq = psutil.cpu_freq()
+if inf.system == "Windows":
+	sy("cls")
+elif inf.system == "Linux":
+	sy("clear")
+#print("\033[33;1m=\033[m"*32)
+print("  ", f"\033[37;1m• System:           \033[34;1m{inf.system}\033[m")
+print("  ", f"\033[37;1m• System version:   \033[36;1m{inf.release}\033[m")
+print("  ", f"\033[37;1m• Architecture:     \033[35m{inf.machine}\033[m")
+print("  ", f"\033[37;1m• RAM:              \033[32;1m{conv(mem.used)} / {conv(mem.total)} ({mem.percent}%)\033[m")
+print("  ", f"\033[37;1m• CPU frequency:    \033[33;1m{frq.current:.2f}Mhz ({psutil.cpu_count(logical=False)} cores)\033[m")
